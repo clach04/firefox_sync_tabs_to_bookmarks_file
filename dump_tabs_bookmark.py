ea213e058f6f21322ad983d26b00d81c44aebda8
clach04 <clach04@gmail.com>
2020-08-01 10:15:25 -0700

    Sanity check for history now fails properly

--- dump_tabs_bookmark.py
+++ dump_tabs_bookmark.py
@@ -144,7 +144,7 @@ def main(argv=None):
                         print(repr(tab["title"]))
                         print(len(tab["urlHistory"]))
                         print(tab["urlHistory"])
-                        raise NotImplemented('lengths != 1 (%r)' % len(tab["urlHistory"]))
+                        raise NotImplementedError('lengths != 1 (%r)' % len(tab["urlHistory"]))
                     url = tab["urlHistory"][0]
                     title = tab["title"]
                     print('''<DT><A HREF="%s" >%s</A>''' % (url, escape(title)))
